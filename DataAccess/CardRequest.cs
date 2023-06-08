21bc989d43928e5081af48bcf97c97a07cd7f935
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2023-06-08 15:02:29 +0330

    Code reviewing.... Not done yet.

--- DataAccess/CardRequest.cs
+++ DataAccess/CardRequest.cs
@@ -7,33 +7,79 @@ namespace MobileBank.DataAccess
 {
     internal class CardRequest: ICardRequest
     {
-        private IDbHelper _dbHelper;
+        //! IDE0044 Make field readonly
+        //x private IDbHelper _dbHelper;
+        private readonly IDbHelper _dbHelper;
+
         public CardRequest(IDbHelper dbHelper)
         {
             _dbHelper = dbHelper;
         }
 
+        //! SQL-Injection is not considered.
+        //public CardInfo FindCard(string card)
+        //{
+        //    string query = $"Select * From CardInfo Where CardNumber= '{card}'";
+
+        //    CardInfo result = null;
+
+        //    SqlDataReader reader = _dbHelper.ExecuteQuery(query);
+
+        //    if (reader.Read())
+        //    {
+        //        result = new CardInfo()
+        //        {
+        //            CardNumber = reader.GetString(1),
+        //            Balance = reader.GetDecimal(2),
+
+        //        };
+        //    }
+        //    reader.Close();
+        //    return result;
+
+        //}
+
+        //! Applying SQL Injection
+        // Rewriting code
+        //public CardInfo FindCard(string card)
+        //{
+        //    string query = $"Select * From CardInfo Where CardNumber= '{card}' OR 1=1; --'";
+
+        //    CardInfo result = null;
+
+        //    SqlDataReader reader = _dbHelper.ExecuteQuery(query);
+
+        //    if (reader.Read())
+        //    {
+        //        result = new CardInfo()
+        //        {
+        //            CardNumber = reader.GetString(1),
+        //            Balance = reader.GetDecimal(2),
+
+        //        };
+        //    }
+        //    reader.Close();
+        //    return result;
+        //}
+
+        //! Optimizing code
         public CardInfo FindCard(string card)
         {
-            string query = $"Select * From CardInfo Where CardNumber= '{card}'";
+            // The `using` statement ensures that the `DataReader` is closed and disposed when it is no longer needed.
+            using var reader = _dbHelper.ExecuteQuery($"Select * From CardInfo Where CardNumber= '{card}' OR 1=1; --'");
 
-            CardInfo result = null;
-
-            SqlDataReader reader = _dbHelper.ExecuteQuery(query);
-
-            if (reader.Read())
-            {
-                result = new CardInfo()
+            //! nullability is not handled.
+            // CS8603 - Possible null reference return.
+            return reader.Read()
+                ? new CardInfo()
                 {
                     CardNumber = reader.GetString(1),
                     Balance = reader.GetDecimal(2),
 
-                };
-            }
-            reader.Close();
-            return result;
-
+                }
+                : null;
         }
+
         public CardInfo FindCardWithId(string Id)
         {
             string query = $"Select * From CardInfo Where Id= '{Id}'";
@@ -56,6 +102,8 @@ namespace MobileBank.DataAccess
             return result;
 
         }
+
+        //! It would be goo to verify whether the `Update` command has successfully updated any record by checking its return value.
         public void UpdateBalance(string id, decimal newBalance)
         {
             string query = $"Update CardInfo Set Balance ='{newBalance}' Where Id='{id}'";
@@ -63,6 +111,8 @@ namespace MobileBank.DataAccess
             _dbHelper.ExecuteNonQuery(query);
 
         }
+        
+        //! Validation checks are not considered.
         public void InsertNewCard(CardInfo newCard)
         {
             string query = $"Insert INTO CardInfo(Id,CardNumber, Balance)Values('{newCard.Id}','{newCard.CardNumber}','{newCard.Balance}')";
--- obj/Debug/net6.0/MobileBank.AssemblyInfo.cs
+++ obj/Debug/net6.0/MobileBank.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MobileBank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7752074358b3be65ca13fe27cfa7afe940b8e50d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MobileBank")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MobileBank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
